b8df2397aaa69ba4c4732d474cce6807eff07a26
Juan Pablo <juanzsalca@outlook.com>
2025-02-10 18:43:13 -0300

    Update settings.py

--- programa/settings.py
+++ programa/settings.py
@@ -24,8 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 TOKEN_CSRF = os.getenv('TOKEN_CSRF')
 SECRET_KEY = TOKEN_CSRF
-# origins = ['https://sitereceitas-production.up.railway.app','localhost','127.0.0.1']
-origins = ['*']
+origins = ['https://sitereceitas-production.up.railway.app','localhost','127.0.0.1']
+
 CSRF_TRUSTED_ORIGINS = origins
 DEBUG = True
 
